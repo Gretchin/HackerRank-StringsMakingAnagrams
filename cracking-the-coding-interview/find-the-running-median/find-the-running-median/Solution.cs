ce2ae4d3eb8476b5b3e8e9ff61db25691b096c09
Gretchin <uru-aleksandr@mail.ru>
2017-04-26 20:50:03 +0300

    find-the-running-median was started

---
+++ cracking-the-coding-interview/find-the-running-median/find-the-running-median/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_the_running_median
+{
+    class Solution
+    {
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            List<int> a = new List<int>();
+            for (int a_i = 0; a_i < n; a_i++)
+            {
+                a.Add(Convert.ToInt32(Console.ReadLine()));
+                //Console.WriteLine();
+            }
+            Console.Read();
+        }        
+    }
+}
