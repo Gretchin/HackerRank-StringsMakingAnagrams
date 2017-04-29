06d8ab6d1b8d2b86b97318c899069ecc306c859e
Gretchin <uru-aleksandr@mail.ru>
2017-04-29 09:21:25 +0300

    Solution of find-the-running-median has been added, but
    hackerrank's testcases 3-9 terminated due to timeout

---
+++ cracking-the-coding-interview/find-the-running-median/find-the-running-median/Heap.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_the_running_median
+{
+    public static class Heap
+    {
+        private static int size = 0;
+
+        static void Heapify(List<int> arr, int i)
+        {
+            int left = 2 * i + 1;
+            int right = left + 1;
+            int max = i;
+            if (left < size)
+            {
+                if (arr[left] > arr[max])
+                    max = left;
+                if (right < size)
+                    if (arr[right] > arr[max])
+                        max = right;
+            }
+            if (max != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = temp;
+                Heapify(arr, max);
+            }
+        }
+
+        public static void Sort(List<int> arr)
+        {
+            Build(arr);
+
+            while (size > 0)
+            {
+                size--;
+                int temp = arr[0];
+                arr[0] = arr[size];
+                arr[size] = temp;
+
+                Heapify(arr, 0);
+            }
+        }
+
+        static void Build(List<int> arr)
+        {
+            size = arr.Count;
+            for (int i = (size - 1) / 2; i >= 0; i--)
+            {
+                Heapify(arr, i);
+            }
+        }
+    }
+}
--- cracking-the-coding-interview/find-the-running-median/find-the-running-median/Solution.cs
+++ cracking-the-coding-interview/find-the-running-median/find-the-running-median/Solution.cs
@@ -16,9 +16,13 @@ namespace find_the_running_median
             for (int a_i = 0; a_i < n; a_i++)
             {
                 a.Add(Convert.ToInt32(Console.ReadLine()));
-                //Console.WriteLine();
+                Heap.Sort(a);
+                int temp = a.Count / 2;
+                if (a.Count % 2 == 0)
+                    Console.WriteLine(((a[temp - 1] + a[temp]) / 2.0).ToString(".0"));
+                else
+                    Console.WriteLine((a[temp]).ToString(".0"));
             }
-            Console.Read();
-        }        
+        }
     }
 }
