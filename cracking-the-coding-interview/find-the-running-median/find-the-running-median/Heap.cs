0bf9fcf49ea284fe1462192b52496e8d8e8825e1
Gretchin <uru-aleksandr@mail.ru>
2017-04-29 14:27:25 +0300

    Solution of find-the-running-median has been added
    with two heaps

--- cracking-the-coding-interview/find-the-running-median/find-the-running-median/Heap.cs
+++ cracking-the-coding-interview/find-the-running-median/find-the-running-median/Heap.cs
@@ -6,53 +6,222 @@ using System.Threading.Tasks;
 
 namespace find_the_running_median
 {
-    public static class Heap
+    abstract class Heap
     {
-        private static int size = 0;
+        protected int size;
+        protected List<int> items;
 
-        static void Heapify(List<int> arr, int i)
+        public Heap()
         {
-            int left = 2 * i + 1;
-            int right = left + 1;
-            int max = i;
-            if (left < size)
-            {
-                if (arr[left] > arr[max])
-                    max = left;
-                if (right < size)
-                    if (arr[right] > arr[max])
-                        max = right;
-            }
-            if (max != i)
-            {
-                int temp = arr[i];
-                arr[i] = arr[max];
-                arr[max] = temp;
-                Heapify(arr, max);
-            }
+            size = 0;
+            items = new List<int>();
         }
 
-        public static void Sort(List<int> arr)
+        public int Size { get { return size; } }
+
+        /** @param parentIndex The index of the parent element.
+        @return The index of the left child.
+        **/
+        public int getLeftChildIndex(int parentIndex)
         {
-            Build(arr);
-
-            while (size > 0)
-            {
-                size--;
-                int temp = arr[0];
-                arr[0] = arr[size];
-                arr[size] = temp;
-
-                Heapify(arr, 0);
-            }
+            return 2 * parentIndex + 1;
         }
 
-        static void Build(List<int> arr)
+        /** @param parentIndex The index of the parent element.
+            @return The index of the right child.
+        **/
+        public int getRightChildIndex(int parentIndex)
         {
-            size = arr.Count;
-            for (int i = (size - 1) / 2; i >= 0; i--)
+            return 2 * parentIndex + 2;
+        }
+
+        /** @param childIndex The index of the child element.
+            @return The index of the parent element.
+        **/
+        public int getParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        /** @param index The index of the element you are checking.
+            @return true if the Heap contains enough elements to fill the left child index, 
+                    false otherwise.
+        **/
+        public bool hasLeftChild(int index)
+        {
+            return getLeftChildIndex(index) < size;
+        }
+
+        /** @param index The index of the element you are checking.
+            @return true if the Heap contains enough elements to fill the right child index, 
+                    false otherwise.
+        **/
+        public bool hasRightChild(int index)
+        {
+            return getRightChildIndex(index) < size;
+        }
+
+        /** @param index The index of the element you are checking.
+            @return true if the calculated parent index exists within array bounds
+                    false otherwise.
+        **/
+        public bool hasParent(int index)
+        {
+            return getParentIndex(index) >= 0;
+        }
+
+        /** @param index The index of the element whose child you want.
+            @return the value in the left child.
+        **/
+        public int leftChild(int index)
+        {
+            return items[getLeftChildIndex(index)];
+        }
+
+        /** @param index The index of the element whose child you want.
+            @return the value in the right child.
+        **/
+        public int rightChild(int index)
+        {
+            return items[getRightChildIndex(index)];
+        }
+
+        /** @param index The index of the element you are checking.
+            @return the value in the parent element.
+        **/
+        public int parent(int index)
+        {
+            return items[getParentIndex(index)];
+        }
+
+        /** @param indexOne The first index for the pair of elements being swapped.
+            @param indexTwo The second index for the pair of elements being swapped.
+        **/
+        public void swap(int indexOne, int indexTwo)
+        {
+            int temp = items[indexOne];
+            items[indexOne] = items[indexTwo];
+            items[indexTwo] = temp;
+        }
+
+        public bool isEmpty()
+        {
+            if (size == 0)
+                return true;
+            return false;
+        }
+
+        /** @return The value at the top of the Heap.
+         **/
+        public int peek()
+        {
+            return items[0];
+        }
+
+
+        /** Extracts root element from Heap.
+        **/
+        public int poll()
+        {
+            int item = items[0];
+            items[0] = items[size - 1];
+            size--;
+            items.RemoveAt(size);
+            heapifyDown();
+            return item;
+        }        
+
+        /** @param item The value to be inserted into the Heap. **/
+        public void add(int item)
+        {
+
+            // Insert value at the next open location in heap
+            items.Add(item);
+            size++;
+
+            // Correct order property
+            heapifyUp();
+        }
+
+        /** Swap values down the Heap. **/
+        public abstract void heapifyDown();
+
+        /** Swap values up the Heap. **/
+        public abstract void heapifyUp();
+
+
+    }
+    class MaxHeap : Heap
+    {
+        public override void heapifyDown()
+        {
+            int index = 0;
+            while (hasLeftChild(index))
             {
-                Heapify(arr, i);
+                int smallerChildIndex = getLeftChildIndex(index);
+
+                if (hasRightChild(index)
+                    && rightChild(index) > leftChild(index)
+                )
+                {
+                    smallerChildIndex = getRightChildIndex(index);
+                }
+
+                if (items[index] > items[smallerChildIndex])
+                {
+                    break;
+                }
+                else
+                {
+                    swap(index, smallerChildIndex);
+                }
+                index = smallerChildIndex;
+            }
+        }
+        public override void heapifyUp()
+        {
+            int index = size - 1;
+
+            while (hasParent(index) && parent(index) < items[index])
+            {
+                swap(getParentIndex(index), index);
+                index = getParentIndex(index);
+            }
+        }
+    }
+    class MinHeap : Heap
+    {
+        public override void heapifyDown()
+        {
+            int index = 0;
+            while (hasLeftChild(index))
+            {
+                int smallerChildIndex = getLeftChildIndex(index);
+
+                if (hasRightChild(index) && rightChild(index) < leftChild(index))
+                {
+                    smallerChildIndex = getRightChildIndex(index);
+                }
+
+                if (items[index] < items[smallerChildIndex])
+                {
+                    break;
+                }
+                else
+                {
+                    swap(index, smallerChildIndex);
+                }
+                index = smallerChildIndex;
+            }
+        }
+        public override void heapifyUp()
+        {
+            int index = size - 1;
+
+            while (hasParent(index) && parent(index) > items[index])
+            {
+                swap(getParentIndex(index), index);
+                index = getParentIndex(index);
             }
         }
     }
--- cracking-the-coding-interview/find-the-running-median/find-the-running-median/Solution.cs
+++ cracking-the-coding-interview/find-the-running-median/find-the-running-median/Solution.cs
@@ -12,16 +12,52 @@ namespace find_the_running_median
         static void Main(String[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            List<int> a = new List<int>();
+            MaxHeap max = new MaxHeap();
+            MinHeap min = new MinHeap();
             for (int a_i = 0; a_i < n; a_i++)
             {
-                a.Add(Convert.ToInt32(Console.ReadLine()));
-                Heap.Sort(a);
-                int temp = a.Count / 2;
-                if (a.Count % 2 == 0)
-                    Console.WriteLine(((a[temp - 1] + a[temp]) / 2.0).ToString(".0"));
+                int buff = Convert.ToInt32(Console.ReadLine());
+
+                if (max.isEmpty())
+                {
+                    if (min.isEmpty())
+                        max.add(buff);
+                    else if (buff < min.peek())
+                    {
+                        max.add(buff);
+                    }
+                    else
+                    {
+                        min.add(buff);
+                    }
+                }
+                else if (min.isEmpty())
+                {
+                    if (buff > max.peek())
+                        min.add(buff);
+                    else
+                    {
+                        max.add(buff);
+                    }
+                }
+                // both heaps is't empty
+                else if (buff > max.peek())
+                    min.add(buff);
                 else
-                    Console.WriteLine((a[temp]).ToString(".0"));
+                    max.add(buff);
+                int temp = max.Size - min.Size;
+                if (Math.Abs(temp) > 1)
+                    if (temp > 0)
+                        min.add(max.poll());
+                    else
+                        max.add(min.poll());
+
+                if(max.Size==min.Size)
+                    Console.WriteLine(((max.peek()+min.peek())/2.0).ToString(".0"));
+                else if(temp>0)
+                    Console.WriteLine((max.peek()).ToString(".0"));
+                else
+                    Console.WriteLine((min.peek()).ToString(".0"));
             }
         }
     }
