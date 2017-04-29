d080e9d92d68624dd1639159b92f66b16a9f5b71
Gretchin <uru-aleksandr@mail.ru>
2017-04-29 17:51:46 +0300

    tries-contacts has been started

---
+++ cracking-the-coding-interview/tries-contacts/tries-contacts/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < n; a0++)
+        {
+            string[] tokens_op = Console.ReadLine().Split(' ');
+            string op = tokens_op[0];
+            string contact = tokens_op[1];
+        }
+    }
+}
