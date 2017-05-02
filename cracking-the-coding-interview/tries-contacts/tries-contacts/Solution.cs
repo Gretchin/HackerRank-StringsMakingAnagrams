9562121a607cc6480fdcbe0fe68341c742e428dd
Gretchin <uru-aleksandr@mail.ru>
2017-05-02 12:24:02 +0300

    tries-contact solution with tree of dictionarys has added, but
    recursion in cycle waste too much time

--- cracking-the-coding-interview/tries-contacts/tries-contacts/Solution.cs
+++ cracking-the-coding-interview/tries-contacts/tries-contacts/Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,127 +9,56 @@ class Solution
 {
     class Node
     {
-        Node left, right;
-        string contact;
-
-        public Node() { }
-        public Node(string contact)
+        Dictionary<char, Node> table;
+        bool finish;
+        public Node()
         {
-            this.contact = contact;
-        }
-
-        public Node Left
-        {
-            get { return left; }
-            set { left = value; }
-        }
-        public Node Right
-        {
-            get { return right; }
-            set { right = value; }
-        }
-        public string Contact
-        {
-            get { return contact; }
-            set { contact = value; }
-        }
-
-
-
-        public bool Search(string contact)
-        {
-            Node cur = this;
-            while (cur != null)
-            {
-                if (cur.Contact == contact)
-                    return true;
-                if (cur > contact)
-                    cur = cur.left;
-                else
-                    cur = cur.right;
-            }
-            return false;
-        }
-
-        public bool isStartWith(string part)
-        {
-            for (int i = 0; i < part.Length; i++)
-                if (i >= this.contact.Length || this.contact[i] != part[i])
-                    return false;
-            return true;
-        }
-        public int PartialSearch(string part)
-        {
-            Node cur = this;
-            int count = 0;
-            while (cur !=null && cur.contact != null)
-            {
-                if (cur.isStartWith(part))
-                {
-                    count++;
-                    if(cur.left!=null)
-                        count += cur.left.PartialSearch(part);
-                    if(cur.right!=null)
-                        count += cur.right.PartialSearch(part);
-                    break;
-                }
-                else if (cur > part)
-                    cur = cur.left;
-                else
-                    cur = cur.right;
-            }
-            return count;
+            table = new Dictionary<char, Node>();
+            finish = false;
         }
         public void Add(string contact)
         {
             Node cur = this;
-            while (cur != null)
+            int i;
+            for (i = 0; i < contact.Length; i++)
             {
-                if (cur > contact)
-                    if (cur.left != null)
-                        cur = cur.left;
-                    else
-                    {
-                        cur.left = new Node(contact);
-                        return;
-                    }
+                if (!cur.table.ContainsKey(contact[i]))
+                {
+                    Node temp = new Node();
+                    cur.table.Add(contact[i], temp);
+                    cur = temp;
+                }
                 else
-                    if (cur.right != null)
-                        cur = cur.right;
-                    else
-                    {
-                        cur.right = new Node(contact);
-                        return;
-                    }
+                {
+                    cur = cur.table[contact[i]];
+                    continue;
+                }
             }
+            cur.finish = true;
+        }
+        public int Search(string contact)
+        {
+            Node cur = this;
+            int i;
+            for (i = 0; i < contact.Length; i++)
+            {
+                if (!cur.table.ContainsKey(contact[i]))
+                    return 0;
+                cur = cur.table[contact[i]];
+            }
+            int count = 0;
+            count += Counter(cur);
+
+            return count;
         }
 
-
-        public static bool operator >(Node arrA, string arrB)
+        int Counter(Node cur)
         {
-
-            int sizeA = arrA.contact.Length;
-            int sizeB = arrB.Length;
-            int minSize = Math.Min(sizeA, sizeB);
-            int i = 0;
-            while (i < minSize && arrA.contact[i] == arrB[i])
-                i++;
-            if (i == minSize)
-                return (sizeA > sizeB) ? true : false;
-            return (arrA.contact[i] > arrB[i]) ? true : false;
-        }
-        public static bool operator <(Node arrB, string arrA)
-        {
-
-            int sizeA = arrA.Length;
-            int sizeB = arrB.contact.Length;
-            int minSize = Math.Min(sizeA, sizeB);
-            int i = 0;
-            while (i < minSize && arrA[i] == arrB.contact[i])
-                i++;
-            if (i == minSize)
-                return (sizeA > sizeB) ? true : false;
-            return (arrA[i] > arrB.contact[i]) ? true : false;
+            int count = (cur.finish) ? 1 : 0;
+            if (cur.table.Count != 0)
+                foreach (Node node in cur.table.Values)
+                    count += Counter(node);
+            return count;
         }
     }
 
@@ -141,20 +71,16 @@ class Solution
             string[] tokens_op = Console.ReadLine().Split(' ');
             string op = tokens_op[0];
             string contact = tokens_op[1];
-
             switch (op)
             {
                 case "add":
                     {
-                        if (root.Contact == null)
-                            root.Contact = contact;
-                        else
-                            root.Add(contact);
+                        root.Add(contact);
                         break;
                     }
                 case "find":
                     {
-                        Console.WriteLine(root.PartialSearch(contact));
+                        Console.WriteLine(root.Search(contact));
                         break;
                     }
                 default:
