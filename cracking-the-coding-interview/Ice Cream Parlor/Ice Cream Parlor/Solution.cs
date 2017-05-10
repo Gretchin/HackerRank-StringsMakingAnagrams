443f3a8fbf270375293af46f164cf0e6a3d602a4
Gretchin <uru-aleksandr@mail.ru>
2017-05-10 18:16:17 +0300

    ice-cream-parlor has started

---
+++ cracking-the-coding-interview/Ice Cream Parlor/Ice Cream Parlor/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+        }
+    }
+}
