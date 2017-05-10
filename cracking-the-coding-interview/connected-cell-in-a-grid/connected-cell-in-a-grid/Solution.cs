4397bebb86a54a9ee6e2c89da720451ddd328f95
Gretchin <uru-aleksandr@mail.ru>
2017-05-10 18:40:21 +0300

    connected-cell-in-a-grid has started

---
+++ cracking-the-coding-interview/connected-cell-in-a-grid/connected-cell-in-a-grid/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int m = Convert.ToInt32(Console.ReadLine());
+        int[][] grid = new int[n][];
+        for (int grid_i = 0; grid_i < n; grid_i++)
+        {
+            string[] grid_temp = Console.ReadLine().Split(' ');
+            grid[grid_i] = Array.ConvertAll(grid_temp, Int32.Parse);
+        }
+    }
+}
