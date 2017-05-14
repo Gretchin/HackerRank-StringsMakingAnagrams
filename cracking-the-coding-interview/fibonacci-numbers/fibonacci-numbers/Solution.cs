c95eec1df976a2577e97f659f42a0e99a5dd551e
Gretchin <uru-aleksandr@mail.ru>
2017-05-14 18:31:23 +0300

    fibonacci-numbers: solution with non-recursive function was added

--- cracking-the-coding-interview/fibonacci-numbers/fibonacci-numbers/Solution.cs
+++ cracking-the-coding-interview/fibonacci-numbers/fibonacci-numbers/Solution.cs
@@ -4,17 +4,29 @@ using System.IO;
 
 class Solution
 {
-
-    public static int Fibonacci(int n)
+    public static int FibonacciRecursion(int n)
     {
         if (n < 2)
             return n;
-        return Fibonacci(n - 2) + Fibonacci(n - 1);
+        return FibonacciRecursion(n - 2) + FibonacciRecursion(n - 1);
+    }
+
+    public static int FibonacciCycle(int n)
+    {
+        int a = 0;
+        int b = 1;
+        for (int i = 1; i < n; i++)
+        {
+            int temp = b;
+            b += a;
+            a = temp;
+        }
+        return b;
     }
 
     static void Main(String[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(Fibonacci(n));
+        Console.WriteLine(FibonacciCycle(n));
     }
 }
