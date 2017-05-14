d240b3fd31e8bedbf06b5ca9b78efa54217254da
Gretchin <uru-aleksandr@mail.ru>
2017-05-14 17:01:44 +0300

    shortest-reach-in-a-graph: has been finished: solution with adjacency matrix
    (abstract class Graph; class GraphWithList; class GraphWithMatrix)

--- cracking-the-coding-interview/shortest-reach-in-a-graph/shortest-reach-in-a-graph/Solution.cs
+++ cracking-the-coding-interview/shortest-reach-in-a-graph/shortest-reach-in-a-graph/Solution.cs
@@ -22,15 +22,15 @@ class Solution
         {
             return (que.Count > 0) ? true : false;
         }
-        protected abstract  bool GetFlag(int v);
-        protected abstract  void SetFlag(int v, bool flag);
-        protected abstract  bool HasPrev(int v);
-        protected abstract  int DistPrev(int v);
-        protected abstract  void SetDist(int v, int dist);
-        protected abstract  int GetDist(int v);
+        protected abstract bool GetFlag(int v);
+        protected abstract void SetFlag(int v, bool flag);
+        protected abstract bool HasPrev(int v);
+        protected abstract int DistPrev(int v);
+        protected abstract void SetDist(int v, int dist);
+        protected abstract int GetDist(int v);
         protected abstract void AddAllAdjToQue(int v);
-        public    abstract  void AddEdge(int v, int u);
-        
+        public abstract void AddEdge(int v, int u);
+
         public Graph(bool directed)
         {
             this.directed = directed;
@@ -47,12 +47,12 @@ class Solution
                 int cur = Dequeue();
                 if (GetFlag(cur))
                     continue;
-                SetFlag(cur,true);
+                SetFlag(cur, true);
                 SetDist(cur, (HasPrev(cur)) ? DistPrev(cur) + weight : 0);
                 AddAllAdjToQue(cur);
             }
-            int[] res = new int[V-1];
-            for (int i = 0, k = 0; i < V - 1; i++,k++)
+            int[] res = new int[V - 1];
+            for (int i = 0, k = 0; i < V - 1; i++, k++)
             {
                 if (GetDist(k) == 0)
                     k++;
@@ -160,8 +160,113 @@ class Solution
                 graph[u - 1].AddAdj(v - 1);
         }
     }
-     
+    class GraphWithMatrix : Graph
+    {
+        int[][] graph;
+        Node[] vertex;
+        class Node
+        {
+            public int Prev { get; set; }
+            public bool Flag { get; set; }
+            public int Dist { get; set; }
 
+            public Node()
+            {
+                Prev = -1;
+                Flag = false;
+                Dist = -1;
+            }
+        }
+        public GraphWithMatrix(int n, bool directed)
+            : base(directed)
+        {
+            graph = new int[n][];
+            vertex = new Node[n];
+            for (int i = 0; i < n; i++)
+                vertex[i] = new Node();
+            if (directed)
+                for (int i = 0; i < n; i++)
+                {
+                    graph[i] = new int[i + 1];
+                    for (int j = 0; j < i + 1; j++)
+                        graph[i][j] = 0;
+                }
+            else
+                for (int i = 0; i < n; i++)
+                {
+                    graph[i] = new int[n];
+                    for (int j = 0; j < n; j++)
+                        graph[i][j] = 0;
+                }
+            V = n;
+        }
+
+        protected override bool GetFlag(int v)
+        {
+            return vertex[v].Flag;
+        }
+        protected override void SetFlag(int v, bool flag)
+        {
+            vertex[v].Flag = flag;
+        }
+        protected override bool HasPrev(int v)
+        {
+            return (vertex[v].Prev == -1) ? false : true;
+        }
+        protected override int DistPrev(int v)
+        {
+            return vertex[vertex[v].Prev].Dist;
+        }
+        protected override void SetDist(int v, int dist)
+        {
+            vertex[v].Dist = dist;
+        }
+        protected override int GetDist(int v)
+        {
+            return vertex[v].Dist;
+        }
+        protected override void AddAllAdjToQue(int v)
+        {
+            if (directed)
+            {
+                for (int i = 0; i < V; i++)
+                    if (graph[v][i] == 1)
+                        if (!HasPrev(i))
+                        {
+                            vertex[i].Prev = v;
+                            Enqueue(i);
+                        }
+            }
+            else
+            {
+                for (int i = 0; i < v; i++)
+                    if (graph[v][i] == 1)
+                        if (!HasPrev(i))
+                        {
+                            vertex[i].Prev = v;
+                            Enqueue(i);
+                        }
+                for (int i = v + 1; i < V; i++)
+                    if (graph[i][v] == 1)
+                        if (!HasPrev(i))
+                        {
+                            vertex[i].Prev = v;
+                            Enqueue(i);
+                        }
+            }
+        }
+        public override void AddEdge(int v, int u)
+        {
+            v--; u--;
+            if (directed)
+                graph[v][u] = 1;
+            else
+                if (u > v)
+                    graph[u][v] = 1;
+                else
+                    graph[v][u] = 1;
+        }
+    }
     static void Main(String[] args)
     {
         int q = Convert.ToInt32(Console.ReadLine());
@@ -169,7 +274,7 @@ class Solution
         {
             string[] m_temp = Console.ReadLine().Split(' ');
             int[] m = Array.ConvertAll(m_temp, Int32.Parse);
-            Graph gr = new GraphWithList(m[0], false);
+            Graph gr = new GraphWithMatrix(m[0], false);
             for (int i = 0; i < m[1]; i++)
             {
                 string[] a_temp = Console.ReadLine().Split(' ');
