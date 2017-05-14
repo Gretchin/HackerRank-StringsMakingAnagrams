f3dc8f59d29e08c8396d118b01942da7895b9486
Gretchin <uru-aleksandr@mail.ru>
2017-05-14 19:47:20 +0300

    daviss-staircase: has been finished
    solution has been modified with adding memory block of already calculated sub-staircases

--- cracking-the-coding-interview/daviss-staircase/daviss-staircase/Solution.cs
+++ cracking-the-coding-interview/daviss-staircase/daviss-staircase/Solution.cs
@@ -4,27 +4,42 @@ using System.IO;
 using System.Linq;
 class Solution
 {
-
-    static int Ways(int n)
+    static int StaircaseClimb(int n)
+    {
+        int[] ways = new int[n];
+        ways[0] = 1;
+        return Ways(n, ways);
+    }
+    static int Ways(int n, int[] ways)
     {
         if (n == 0)
             return 1;
         int res = 0;
-        res += Ways(n - 1);
+        if (ways[n - 1] == 0)
+            ways[n - 1] = Ways(n - 1, ways);
+        res += ways[n - 1];
         if (n > 1)
-            res += Ways(n - 2);
+        {
+            if (ways[n - 2] == 0)
+                ways[n - 2] = Ways(n - 2, ways);
+            res += ways[n - 2];
+        }
         if (n > 2)
-            res += Ways(n - 3);
+        {
+            if (ways[n - 3] == 0)
+                ways[n - 3] = Ways(n - 3, ways);
+            res += ways[n - 3];
+        }
         return res;
     }
-
     static void Main(String[] args)
     {
         int s = Convert.ToInt32(Console.ReadLine());
         for (int a0 = 0; a0 < s; a0++)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Ways(n));
+            Console.WriteLine(StaircaseClimb(n));
         }
+        Console.Read();
     }
 }
