92559a024740c660e47c95ce0750650e61a9d360
Gretchin <uru-aleksandr@mail.ru>
2017-05-14 18:45:41 +0300

    daviss-staircase: recursive solution has been added, but
    it was terminated due to timeout in some testcases

--- cracking-the-coding-interview/daviss-staircase/daviss-staircase/Solution.cs
+++ cracking-the-coding-interview/daviss-staircase/daviss-staircase/Solution.cs
@@ -5,12 +5,26 @@ using System.Linq;
 class Solution
 {
 
+    static int Ways(int n)
+    {
+        if (n == 0)
+            return 1;
+        int res = 0;
+        res += Ways(n - 1);
+        if (n > 1)
+            res += Ways(n - 2);
+        if (n > 2)
+            res += Ways(n - 3);
+        return res;
+    }
+
     static void Main(String[] args)
     {
         int s = Convert.ToInt32(Console.ReadLine());
         for (int a0 = 0; a0 < s; a0++)
         {
             int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Ways(n));
         }
     }
 }
