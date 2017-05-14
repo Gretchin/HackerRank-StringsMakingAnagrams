ca4a76f984f1bbddc33945e035901890d956b360
Gretchin <uru-aleksandr@mail.ru>
2017-05-14 17:12:48 +0300

    time-complexity-primality: has started

---
+++ cracking-the-coding-interview/time-complexity-primality/time-complexity-primality/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int p = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < p; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
