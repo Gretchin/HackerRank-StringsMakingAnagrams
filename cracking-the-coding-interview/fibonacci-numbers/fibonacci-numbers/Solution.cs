23214d001b2a68fa08665d7bba6042c60403863f
Gretchin <uru-aleksandr@mail.ru>
2017-05-14 18:17:35 +0300

    fibonacci-numbers: has started

---
+++ cracking-the-coding-interview/fibonacci-numbers/fibonacci-numbers/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+
+    public static int Fibonacci(int n)
+    {
+
+        // Write your code here.
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Fibonacci(n));
+    }
+}
