54acb7ad069e3e01e9a2f9a34cf0d1980d3d6268
Gretchin <uru-aleksandr@mail.ru>
2017-05-15 18:14:15 +0300

    bit-manipulation-lonely-integer: has been finished
    with a hint from discussions

--- cracking-the-coding-interview/bit-manipulation-lonely-integer/bit-manipulation-lonely-integer/Solution.cs
+++ cracking-the-coding-interview/bit-manipulation-lonely-integer/bit-manipulation-lonely-integer/Solution.cs
@@ -4,11 +4,21 @@ using System.IO;
 using System.Linq;
 class Solution
 {
+    static int ForeverAlone(int[] a,int n)
+    {
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res ^= a[i];
+        }
+        return res;
+    }
 
     static void Main(String[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine());
         string[] a_temp = Console.ReadLine().Split(' ');
         int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+        Console.WriteLine(ForeverAlone(a, n));
     }
 }
